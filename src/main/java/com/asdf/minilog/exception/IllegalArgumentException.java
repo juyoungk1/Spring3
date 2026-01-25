c153ac6050f1adb2913ebae431c63e0c3d4874aa
youngk <soakaeofhg@naver.com>
2026-01-25 22:38:07 +0900

    2026/01/25

---
+++ src/main/java/com/asdf/minilog/exception/IllegalArgumentException.java
@@ -0,0 +1,7 @@
+package com.asdf.minilog.exception;
+
+public class IllegalArgumentException extends RuntimeException {
+    public IllegalArgumentException(String message) {
+        super(message);
+    }
+}
